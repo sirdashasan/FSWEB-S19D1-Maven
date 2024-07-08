5f873550f861224e8b816b54afccccb94c225cca
sirdashasan <sirdashasan@hotmail.com>
2024-07-08 21:48:21 +0300

    Tüm görevler tamamlandı

---
+++ src/main/java/com/workintech/s18d2/entity/Vegetable.java
@@ -0,0 +1,21 @@
+package com.workintech.s18d2.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "vegetable", schema = "fsweb")
+public class Vegetable extends Plant{
+
+    @Column(name = "is_grown_on_tree")
+    private boolean isGrownOnTree;
+
+
+
+
+}
